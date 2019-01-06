68965383d5971cfcc510f6359844d2cec3f60683
renaldiap <44296686+renaldiap@users.noreply.github.com>
2019-01-06 15:16:46 +0700

    Update user.php

--- user/user.php
+++ user/user.php
@@ -1,13 +1,13 @@
 <?php
 $stat_sseth=true;
-$msgsseth= "[---] enjoy your life";
+$msgsseth= "[---] selamat menuyul";
 $stat_cp = true;
-$msgcp = "[--] enjoy your life";
-$figlet= $ijo."
+$msgcp = "[--] jangan serakah";
+$figlet= $biru."
 ───────────────────────────────────────────
-── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$kuning." [`  ||    _    _.       
+── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$biru." [`  ||    _    _.       
 ── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
 ── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
-  $putih."creator:".$ijo." adidoank   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
-  $putih."chanel :".$ijo." adi bordir ".$putih."[--] chanel:".$ijo." sungging  ".$t.
-  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo."  otr3o8   ".$t;
+  $putih."creator:".$merah." RenaldiAP   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
+  $putih."Wa :".$ijo." 089674381994".$putih."[--] chanel:".$ijo." sungging  ".$t.
+  $putih."invite :".$ijo." No Edit     ".$putih."[--] invite:".$ijo."  otr3o8   ".$t;
